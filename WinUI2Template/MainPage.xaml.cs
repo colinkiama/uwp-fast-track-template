b335d0d97b5ca49be4f07517edfa708d8751860a
Colin Kiama <colinkiama@hotmail.co.uk>
2020-12-19 14:05:31 +0000

    VSIX project included

--- WinUI2Template/MainPage.xaml.cs
+++ WinUI2Template/MainPage.xaml.cs
@@ -9,7 +9,7 @@ using Windows.UI.Xaml.Navigation;
 using muxc = Microsoft.UI.Xaml.Controls;
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace WinUI2Template
+namespace $safeprojectname$
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
