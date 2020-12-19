b0470352332e9f34a020e47f34b0410586479691
Colin Kiama <colinkiama@hotmail.co.uk>
2020-12-19 14:18:51 +0000

    Renamed for template generation

--- UWPFastTrackTemplate.UWP/View/Page1.xaml.cs
+++ UWPFastTrackTemplate.UWP/View/Page1.xaml.cs
@@ -17,7 +17,7 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace UWPFastTrackTemplate.UWP.View
+namespace $safeprojectname$
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
--- WinUI2Template/HomePage.xaml.cs
+++ WinUI2Template/HomePage.xaml.cs
@@ -1,10 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using UWPFastTrackTemplate.ViewModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,15 +20,11 @@ namespace $safeprojectname$
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class SettingsView : Page
+    public sealed partial class HomePage : Page
     {
-        public SettingsView()
+        public HomePage()
         {
             this.InitializeComponent();
-            this.DataContext = App.Services.GetRequiredService<SettingsViewModel>();
-
         }
-
-        SettingsViewModel ViewModel => (SettingsViewModel)this.DataContext;
     }
 }
