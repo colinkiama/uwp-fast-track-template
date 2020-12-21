32a4b0941988a76f8b0627ccfc1cd53ef9924f96
Colin Kiama <colinkiama@hotmail.co.uk>
2020-12-21 00:51:07 +0000

    feat: Added all necessary packages

---
+++ UWPFastTrackTemplate/Services/INavigationService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UWPFastTrackTemplate.Services
+{
+    interface INavigationService
+    {
+    }
+}
---
+++ UWPFastTrackTemplate/ViewModel/NavigatableViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UWPFastTrackTemplate.ViewModel
+{
+    public abstract class NavigatableViewModelBase: ViewModelBase
+    {
+        //INavigationService
+    }
+}
