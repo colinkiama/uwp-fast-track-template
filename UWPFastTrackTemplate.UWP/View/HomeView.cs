dbb874b01276c233cdbfa7f98237a9a24aa87189
Colin Kiama <colinkiama@hotmail.co.uk>
2020-12-23 02:02:21 +0000

    Projects no longer reference each other by default
    Users have to link the projects themselves when the use the template

--- UWPFastTrackTemplate.UWP/View/HomeView.cs
+++ UWPFastTrackTemplate.UWP/View/HomeView.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Numerics;
 using System.Threading.Tasks;
-using $safeprojectname$.ViewModel;
+using $ext_safeprojectname$.ViewModel;
 using Windows.UI.Composition;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -10,7 +10,7 @@ using Windows.UI.Xaml.Hosting;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace $safeprojectname$.UWP.View
+namespace $ext_safeprojectname$.UWP.View
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
